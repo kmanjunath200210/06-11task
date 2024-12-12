c01fd4f7aca8bfd847a0f685f43ab066a05c9308
unknown <kanikemanjunath200210@gmaail.com>
2024-12-12 22:01:24 +0530

    frist

---
+++ js.js
@@ -0,0 +1,112 @@
+
+
+const Card1=document.querySelector(".div1");
+Card1.id="card"
+Card1.innerHTML=`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiDnqM51geO09ruMdLEx01sUVashepTGAcFw&s" width="300" height="200"/>
+<p>PROMEGRANATE FRUIT</p>
+<button>MORE EDINTING APPS</button>
+`
+Card1.style.border="2px solid black"
+Card1.style.backgroundColor="lightgray"
+Card1.style.display="inline-block"
+Card1.style.padding="20px"
+
+
+
+
+
+const Card2=document.querySelector(".div2");
+Card2.id="card"
+Card2.innerHTML=`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg5huVjI7d2JQofon4d8lddUkiFxLxBcSKwg&s" width="300" height="200"/>
+<p>GRAPES FRUIT</p>
+<button>MORE EDINTING APPS</button>
+
+
+`
+
+Card2.style.border="2px solid black"
+Card2.style.backgroundColor="lightgray"
+Card2.style.display="inline-block"
+Card2.style.padding="20px"
+
+
+
+
+const Card3=document.querySelector(".div3");
+Card3.id="card"
+Card3.innerHTML=`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf6eluXzxsKXp3MLH4OhirwCrPm2Ivc4hxGQ&s" width="300" height="200"/>
+<p>ORANGE FRUIT</p>
+<button>MORE EDINTING APPS</button>
+
+
+`
+
+Card3.style.border="2px solid black"
+Card3.style.backgroundColor="lightgray"
+Card3.style.display="inline-block"
+Card3.style.padding="20px"
+
+const newCard1=document.createElement("div");
+newCard1.id="card"
+
+newCard1.innerHTML=`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiZcBq4DZml42mRKXAUuIJ5Anu0WfL19GYsA&s" width="300" height="200"/>
+
+<p>APPLE FRUIT</P>
+<button>PHOTO EDITING APPS</BUTTON>
+
+
+
+`
+
+newCard1.style.border="2px solid black"
+newCard1.style.backgroundColor="lightgray"
+newCard1.style.display="inline-block"
+newCard1.style.padding="20px"
+console.log(newCard1);
+document.body.appendChild(newCard1)
+
+
+const newCard5=document.createElement("div");
+newCard5.id="card"
+
+newCard5.innerHTML=`<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSyyelwJR2SuDRn9hSS3Z9OepMqTSuVomz6w&s" width="300" height="200"/>
+
+<p>KIWI FRUIT</P>
+<button>PHOTO EDITING APPS</BUTTON>
+
+`
+
+newCard5.style.border="2px solid black"
+newCard5.style.backgroundColor="lightgray"
+newCard5.style.display="inline-block"
+newCard5.style.padding="20px"
+console.log(newCard5);
+document.body.appendChild(newCard5)
+
+
+
+
+const newCard6=document.createElement("div");
+newCard6.id="card"
+
+newCard6.innerHTML=`<img src="https://static.vecteezy.com/system/resources/thumbnails/036/007/101/small/red-pear-fruits-covered-with-water-droplets-generate-ai-photo.jpg" width="300" height="200"/>
+
+<p>PHOTO EDITING APP</P>
+<button>PHOTO EDITING APPS</BUTTON>
+
+`
+
+newCard6.style.border="2px solid black"
+newCard6.style.backgroundColor="lightgray"
+newCard6.style.display="inline-block"
+newCard6.style.padding="20px"
+console.log(newCard6);
+document.body.appendChild(newCard6)
+
+
+
+
+
+
+
+
